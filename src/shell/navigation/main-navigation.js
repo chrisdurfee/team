2a39e44e2c2491e34bdf58ab97075274a0036296
tech_e <chris.durfee@dentalqore.com>
2025-02-22 16:06:21 -0700

    refactor: remove LowerNavigation component and clean up main navigation structure

--- src/shell/navigation/main-navigation.js
+++ src/shell/navigation/main-navigation.js
@@ -1,9 +1,7 @@
 import { Div } from "@base-framework/atoms";
 import { Atom, Component } from "@base-framework/base";
-import { Icons } from "@base-framework/ui/icons";
 import { InlineNavigation } from "@base-framework/ui/organisms";
 import { Configs } from "../../configs.js";
-import { NavigationAvatar } from "./avatars/navigation-avatar.js";
 import { MainHeader } from "./main-header.js";
 import { ShortNavigation } from "./short/short-navigation.js";
 
@@ -31,37 +29,6 @@ const PrimaryNavigation = ({ options }) => (
 );
 
 /**
- * This will create the lower navigation.
- *
- * @returns {object}
- */
-const LowerNavigation = () => (
-	Div([
-		new InlineNavigation({
-			options: [
-				{
-					content: [
-
-						/**
-						 * This will create the navigation avatar.
-						 */
-						new NavigationAvatar({
-							data: app.data.user
-						})
-					],
-					callBack: () => app.navigate('settings/profile')
-				},
-				{
-					href: 'settings',
-					label: 'Settings',
-					icon: Icons.cog.eight
-				}
-			]
-		})
-	])
-);
-
-/**
  * MainNavigation
  *
  * This will create the main navigation.
@@ -84,8 +51,7 @@ export class MainNavigation extends Component
 				PrimaryNavigation({
 					// @ts-ignore
 					options: this.options
-				}),
-				LowerNavigation()
+				})
 			])
 		]);
 	}
